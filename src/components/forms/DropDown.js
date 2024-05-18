6044ecb4daf054e6b0cae1c99e045cfcd96f508d
minseong99 <rlacjfks123@gmail.com>
2024-05-18 17:01:28 +0900

    feat: dropdown버튼 구현
    -유형(식료품, 스포츠 등)을 선택할 수 있는 dropdown버튼의 형태를 만들고 기능까지 만들었습니다.

--- src/components/forms/DropDown.js
+++ src/components/forms/DropDown.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import "./DropDown.css";
+
+const typeList = [
+  "식료품",
+  "패션의류/잡화",
+  "뷰티",
+  "유아동",
+  "주방용품",
+  "생활용품",
+  "스포츠",
+];
 
 const DropDown = () => {
+  const [showOption, setShowOption] = useState(false);
+  const [type, setType] = useState("");
+
+  const handleOnclick = () => {
+    setShowOption((preState) => !preState);
+  };
+  const handleOnclickOption = (type) => {
+    setType(type);
+    setShowOption(false);
+  };
+
+  const renderOptions = typeList.map((type, index) => {
+    return (
+      <div
+        class="option-type"
+        onClick={() => handleOnclickOption(type)}
+        key={index}
+      >
+        {type}
+      </div>
+    );
+  });
+
   return (
-    <div>
+    <div class="dropdown">
       <label for="type">유형</label>
-      {/* 드롭다운구현 */}
-      <div>식료품</div>
+      <div class="form-option">
+        <input
+          class="button-option"
+          onClick={handleOnclick}
+          type="button"
+          value={type ? type : ""}
+        ></input>
+        {/* 드롭다운구현 */}
+        {showOption && <>{renderOptions}</>}
+      </div>
     </div>
   );
 };
