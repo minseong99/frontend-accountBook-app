ae73dab549868b6d36fcc8ac17110431cc17f1e7
minseong99 <rlacjfks123@gmail.com>
2024-05-21 20:45:21 +0900

    fix: 기간의 범위가 커지면 적용이 안되는 문제 해결
    -기간 필터링을 키우면 제대로 적용이 안되는 문제를 해결하였습니다.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import { useState } from "react";
 function App() {
   const [showList, setShowList] = useState([]);
   const [renderList, setRenderList] = useState([]);
-  console.log(showList);
-  console.log(renderList);
 
   const config = [
     {
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,17 +1,23 @@
 import "./ItemList.css";
-// import { useState } from "react";
+import { useEffect, useState } from "react";
 import FilterAndSortBy from "./FilterAndSortBy";
 
 const ItemList = (props) => {
-  // const [sortBy, setSortBy] = useState(null);
-  // const [filterType, setFilterType] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+  const [filterType, setFilterType] = useState(null);
+  const [startFilter, setStartFilter] = useState(null);
+  const [endFilter, setEndFilter] = useState(null);
 
-  // const [startFilter, setStartFilter] = useState(null);
-  // const [endFilter, setEndFilter] = useState(null);
+  useEffect(() => {
+    filterByType(filterType);
+    sort(sortBy);
+    filterByTerm(startFilter, endFilter);
+  }, [filterType, sortBy, startFilter, endFilter]);
 
   // 정렬기준에 따른 기준
   const sort = (data) => {
     if (data === null) return;
+
     // data는 list의 value값
     const { findTarget } = props.sortList.find(
       (column) => column.value === data
@@ -30,33 +36,49 @@ const ItemList = (props) => {
       }
     });
     props.setRenderList(sortedList);
-    // setSortBy(data);
+    console.log(props.renderList);
+
+    setSortBy(data);
   };
 
   // 유형에 따른 필터링
   const filterByType = (data) => {
     if (data === null) return;
+
     // data는 list의 value값
     const filteredList = props.showList.filter((item) => item.type === data);
 
     props.setRenderList(filteredList);
-    // setFilterType(data);
+
+    setFilterType(data);
   };
 
   // 기간 선택에 따른 필터링
   const filterByTerm = (start, end) => {
     if (!start || !end) return;
+    const startTerm = new Date(start).getTime();
+    const endTerm = new Date(end).getTime();
 
-    const startTerm = start ? new Date(start).getTime() : new Date(0).getTime();
-    const endTerm = end ? new Date(end).getTime() : new Date().getTime();
+    // 범위가 작은것에서 커질때 안나타나는 현상을 해결하기 위해
+    if (startTerm < startFilter || endTerm > endFilter) {
+      filterByType(filterType);
+
+      setStartFilter(startTerm);
+      setEndFilter(endTerm);
+      return;
+    }
+
+    // 시작기간을 더 늦게 설정할경우 종료
     if (startTerm > endTerm) return;
-    const filteredByTermList = props.showList.filter((itemInfo) => {
+    const filteredByTermList = props.renderList.filter((itemInfo) => {
       const itemDateTime = new Date(itemInfo.date).getTime();
       return itemDateTime >= startTerm && itemDateTime <= endTerm;
     });
+
     props.setRenderList(filteredByTermList);
-    // setStartFilter(start);
-    // setEndFilter(end);
+
+    setStartFilter(startTerm);
+    setEndFilter(endTerm);
   };
 
   return (
